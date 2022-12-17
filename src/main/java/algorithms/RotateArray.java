2c3af91e4e72fa23e9e99292aaf8c2d273645aca
Decagon <decagon@Decagons-MacBook-Air-4.local>
2022-12-17 14:20:03 +0100

    project modified

--- src/main/java/algorithms/RotateArray.java
+++ src/main/java/algorithms/RotateArray.java
@@ -10,12 +10,16 @@ How many different ways do you know to solve this problem?
 
 import java.util.Arrays;
 
+//FOR THIS SOLUTION THE SPACE COMPLEXITY IS O(n) AND TIME COMPLEXITY IS O(n)
 public class RotateArray {
     public static void main(String[] args) {
 //    to print out the result u have to loop
         int[] arryResult = rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
         for (int i = 0; i < arryResult.length - 1; i++) {
             System.out.print(arryResult[i] + " ");
+
+            //method 2
+            method2(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
         }
     }
 
@@ -25,12 +29,28 @@ public class RotateArray {
         for (int i = 0; i < k; i++) {
             result[i] = nums[nums.length - k + i];
         }
-        System.out.println(Arrays.toString(result));
+//        System.out.println(Arrays.toString(result));
         int j = 0;
         for (int i = k; i < nums.length - 1; i++) {
             result[i] = nums[j];
             j++;
         }
         return result;
+    }
+
+    //BUBBLE METHOD ADDED
+    public static int[] method2(int[] arr, int order) {
+        if (arr == null || order < 0) {
+            throw new IllegalArgumentException("illegalArgumentExeception");
+        }
+
+        for (int i = 0; i < order; i++) {
+            for (int j = arr.length - 1; j > 0; j--) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+        return arr;
     }
 }
